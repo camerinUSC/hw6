146944698a8ec1f9bb34867d4b6efd23738fdad7
Camerin Lee <camerinl@usc.edu>
2025-05-04 23:11:19 +0000

    part 1 completed

--- hash.h
+++ hash.h
@@ -21,13 +21,86 @@ struct MyStringHash {
     {
         // Add your code here
 
+				std::string mainStr = "";
+				int length = k.length();
 
+				//filtering given parameter 
+				for(int x = 0; x < length; x++){
+					char currentChar = k[x];
+						if(std::isdigit(currentChar)){
+							mainStr += currentChar;
+						}
+
+						else if(std::isalpha(currentChar)){
+							mainStr += currentChar;
+						}
+				}
+
+				int filteredLength = mainStr.length();
+
+				//splitting into 6 groups
+				HASH_INDEX_T word[5];
+
+				for(int x = 0; x < 5; x++){
+					word[x] = 0;
+				}
+
+
+				//hashing
+
+				int indexSplit = 4;
+				for(int x = filteredLength; x > 0 && indexSplit >= 0; x-=6){
+
+					HASH_INDEX_T val = 0;
+
+					int splitStart = x-6; 		//account for splits smaller than 6
+					if(splitStart < 0){
+						splitStart = 0;
+					}
+
+					// hashing operation (powers of 36)
+
+					for(int y = splitStart; y < x; y++){
+
+						char current = mainStr[y];
+						HASH_INDEX_T currentVal = letterDigitToNumber(current);
+						val = val * 36 + currentVal;
+
+					}
+
+					word[indexSplit] = val;
+					indexSplit--;
+				}
+
+			//combining the 6 parts 
+
+			HASH_INDEX_T finished_product = 0;
+
+			for(int x = 0; x < 5; x++){
+				finished_product += rValues[x] * word[x];
+			}
+
+
+			return finished_product;
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
+
+				bool varifiedLetter = std::isalpha(letter);
+				bool varifiedDigit = std::isdigit(letter);
+
+				if(varifiedLetter){
+					char lowercase = std::tolower(letter);
+					return static_cast<HASH_INDEX_T>(lowercase - 'a');
+				}
+
+				else if(varifiedDigit){
+					return static_cast<HASH_INDEX_T>((letter - '0')+26);
+				}
+	
 
     }
 
